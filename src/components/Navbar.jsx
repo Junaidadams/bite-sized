1f7dfc2e091fd70247734d682c7b952b7b9ac2cc
Junaid Adams <junaidadams117@gmail.com>
2024-09-16 15:08:13 +0200

    small changes to home and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,16 @@
 import { navigationLinks } from "../../constants";
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <ul>
+        {navigationLinks.map((link) => (
+          <li key={link.key}>
+            <a href={link.link}>{link.name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
